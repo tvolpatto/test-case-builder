ca9464895158361c06a852046ead763d22c5ae36
Thyago Volpatto <tvolpatto@gmail.com>
2021-05-03 13:55:39 +0000

    update schema

--- models/index.js
+++ models/index.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const marketSchema = new Schema({
+  id: { type: String, required: true }
   name: { type: String, required: true }
 });
 
 const mainSchema = new Schema({
   name: { type: String, required: true },
-  market_id: { type: Schema.Types.ObjectId, ref: "Market" } 
 });
 
 const caseSchema = new Schema({
@@ -19,13 +19,16 @@ const caseSchema = new Schema({
 const workflowSchema = new Schema({
   number: { type: String, required: true },
   market_id: { type: Schema.Types.ObjectId, ref: "Market" } ,
+  main_id: { type: Schema.Types.ObjectId, ref: "Main" },
 
 });
 
 const testResultsSchema = new Schema({
   number: { type: String, required: true },
   workflow_id: { type: Schema.Types.ObjectId, ref: "Workflow" } ,
-
+  case_id: { type: Schema.Types.ObjectId, ref: "Case" },
+  result: { type: String},
+  note: { type: String}
 });
 
 
